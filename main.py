856fae4d7cb64dd89784828c85fe40e2e6aafeeb
Rucky0 <zugo789@hotmail.com>
2024-09-27 21:30:00 +0200

    updated main

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ import os
 
 app = Flask(__name__)
 
-@app.route('/recipe_generator', methods=['POST'])
-def recipe_generator(jsonquery):
+#Step 1
+@app.route('/generate_recipe', methods=['POST'])
+def generate_recipe(query):
     try:
         
         return jsonify({"message": "Hello recipe!"}), 200
@@ -13,17 +14,8 @@ def recipe_generator(jsonquery):
         return jsonify({"error": str(e)}), 500
 
 
-@app.route('/get_ingredients_prices', methods=['POST'])
-def get_ingredients_prices():
-    try:
-        return jsonify({"message": "Hello Ingredients!"}), 200
-
-    except Exception as e:
-        return jsonify({"error": str(e)}), 500
-
-
 @app.route('/checkout', methods=['POST'])
-def openai_query():
+def openai_query(ingredients):
     try:
         return jsonify({"message": "Hello checkout!"}), 200
 
